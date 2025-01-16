e6a2e6724cac4cb0a034f9780c61b375f16af593
Rawan-Rawan <rawansalahat1@gmail.com>
2025-01-16 20:56:20 +0200

    done

---
+++ Assets/Assign 35/Scripts/part2/MulticastDelegateExample.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void MathOperation(int number);
+public class MulticastDelegateExample : MonoBehaviour
+{
+    void Start()
+    {
+        MathOperation operation = null;
+        operation += DoubleNumber;
+        operation += SquareNumber;
+        operation += CubeNumber;
+
+        int inputValue = 5;
+        operation(inputValue);
+    }
+    void DoubleNumber(int number)
+    {
+        int doubleNumber = number * 2;
+        Debug.Log($"Double:{doubleNumber}");
+    }
+    void SquareNumber(int number)
+    {
+        int squareNumber = number * number;
+        Debug.Log($"Square:{squareNumber}");
+    }
+    void CubeNumber(int number)
+    {
+        int cubeNumber = number * number * number;
+        Debug.Log($"Cube:{cubeNumber}");
+    }
+}
