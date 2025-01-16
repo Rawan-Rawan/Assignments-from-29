2cea70524800a203a1a8cada533fdb30e0b05592
Rawan-Rawan <rawansalahat1@gmail.com>
2025-01-16 20:59:00 +0200

    done

--- Assets/Assign 35/Scripts/part2/MulticastDelegateExample.cs
+++ Assets/Assign 35/Scripts/part2/MulticastDelegateExample.cs
@@ -1,33 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public delegate void MathOperation(int number);
-public class MulticastDelegateExample : MonoBehaviour
+namespace Assignment35
 {
-    void Start()
+    public delegate void MathOperation(int number);
+    public class MulticastDelegateExample : MonoBehaviour
     {
-        MathOperation operation = null;
-        operation += DoubleNumber;
-        operation += SquareNumber;
-        operation += CubeNumber;
+        void Start()
+        {
+            MathOperation operation = null;
+            operation += DoubleNumber;
+            operation += SquareNumber;
+            operation += CubeNumber;
 
-        int inputValue = 5;
-        operation(inputValue);
-    }
-    void DoubleNumber(int number)
-    {
-        int doubleNumber = number * 2;
-        Debug.Log($"Double:{doubleNumber}");
-    }
-    void SquareNumber(int number)
-    {
-        int squareNumber = number * number;
-        Debug.Log($"Square:{squareNumber}");
-    }
-    void CubeNumber(int number)
-    {
-        int cubeNumber = number * number * number;
-        Debug.Log($"Cube:{cubeNumber}");
+            int inputValue = 5;
+            operation(inputValue);
+        }
+        void DoubleNumber(int number)
+        {
+            int doubleNumber = number * 2;
+            Debug.Log($"Double:{doubleNumber}");
+        }
+        void SquareNumber(int number)
+        {
+            int squareNumber = number * number;
+            Debug.Log($"Square:{squareNumber}");
+        }
+        void CubeNumber(int number)
+        {
+            int cubeNumber = number * number * number;
+            Debug.Log($"Cube:{cubeNumber}");
+        }
     }
 }
