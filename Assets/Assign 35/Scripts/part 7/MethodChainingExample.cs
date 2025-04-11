bb6d7b5170fd5358363e620720532def5bce12a9
Rawan-Rawan <rawansalahat1@gmail.com>
2025-04-12 00:38:14 +0300

    done

--- Assets/Assign 35/Scripts/part 7/MethodChainingExample.cs
+++ Assets/Assign 35/Scripts/part 7/MethodChainingExample.cs
@@ -5,11 +5,13 @@ namespace Assignment35
 {
     public class MethodChainingExample : MonoBehaviour
     {
+
         public MethodChainingExample SetPosition(Vector3 position)
         {
             transform.position = position;
             return this;
         }
+
 
         public MethodChainingExample SetRotation(Vector3 rotation)
         {
@@ -17,16 +19,17 @@ namespace Assignment35
             return this;
         }
 
+
         public MethodChainingExample SetScale(Vector3 scale)
         {
             transform.localScale = scale;
             return this;
         }
+
+
         void Start()
         {
-            SetPosition(new Vector3(0, 1, 0));
-            SetRotation(new Vector3(45, 0, 0));
-            SetScale(new Vector3(2, 2, 2));
+            SetPosition(new Vector3(0, 1, 0)).SetRotation(new Vector3(45, 0, 0)).SetScale(new Vector3(2, 2, 2));
         }
     }
 }
